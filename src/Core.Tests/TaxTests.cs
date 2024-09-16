dcb94d2cd1fd056bee4e1f2149be10f692259c02
Evan Gray <Evanoo_g@outlook.com>
2024-09-16 19:52:23 +0100

    added bacic taxcalc

---
+++ src/Core.Tests/TaxTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Tests
+{
+    public class TaxTests
+    {
+        [Theory]
+        [InlineData(10000, 0)]
+        [InlineData(5000, 0)]
+        [InlineData(13500, 200)]
+        [InlineData(16500, 800)]
+        [InlineData(50000, 7500)]
+        [InlineData(60000, 11500)]
+        [InlineData(70000,15_500)]
+        [InlineData(100_000,27500)]
+        public void testtax(decimal input, decimal output)
+        {
+            var taxCalc = new TaxCalc();
+            var o = taxCalc.Calc(input);
+            Assert.Equal(output, o);
+        }
+    }
+}
---
+++ src/Core/TaxCalc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    internal class TaxCalc
+    {
+
+        public decimal Calc(decimal input)
+        {
+
+            if (input <= 12500)
+            {
+                return 0;
+            }
+
+            decimal moneyWeCanTaxAt40pc = 0;
+            decimal moneyWeCanTaxAt20pc = 0;
+
+
+            moneyWeCanTaxAt40pc = input - 50_000;
+            if (moneyWeCanTaxAt40pc < 0)
+            {
+                moneyWeCanTaxAt40pc = 0;
+            }
+
+            if (input > 50_000)
+            { moneyWeCanTaxAt20pc = 37_500; }
+
+            if (input <= 50_000)
+            {
+                moneyWeCanTaxAt20pc = input - 12_500;
+            }
+
+            return (moneyWeCanTaxAt20pc * 0.2M) + (moneyWeCanTaxAt40pc * .4M);
+
+
+        }
+    }
+}
